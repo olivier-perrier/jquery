6fbcdc1d6a852fc6febc53acfe217433cc502f3f
PERRIER Olivier <olivier.perrier@soprasteria.com>
2018-11-23 17:59:21 +0100

    admin

--- models/Post.js
+++ models/Post.js
@@ -6,12 +6,19 @@ var Post = {
 }
 
 function createPost(name, content) {
-    data.posts.insert({name: name, content: content}, (err, doc) => {
+    data.posts.insert({
+        name: name,
+        content: content,
+        createdAt: new Date()
+    }, (err, doc) => {
         console.log('Post created')
     })
 
 }
 
+// data.posts.update({}, {$set: {createdAt: new Date()}})
+
+// createPost("Post 3", "Bla bla blabla blaaaaa <strong>strong</strong>")
 // createPost("Post 3", "Bla bla blabla blaaaaa <strong>strong</strong>")
 
 module.exports = Post
--- routes/posts.js
+++ routes/posts.js
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
 })
 
 router.get('/:postId', (req, res) => {
-  console.log("GET /posts")
+  console.log("GET /posts/:postId")
   var postId = req.params.postId
 
   data.posts.findOne({_id: postId}, (err, doc) => {
@@ -26,6 +26,20 @@ router.get('/:postId', (req, res) => {
       res.send({ message: "not found : no posts found" })
     } else {
       res.render('post', {post: doc})
+    }
+  })
+
+})
+
+router.get('/edit/:postId', (req, res) => {
+  console.log("GET /posts/edit/:postId")
+  var postId = req.params.postId
+
+  data.posts.findOne({_id: postId}, (err, doc) => {
+    if (doc == null) {
+      res.send({ message: "not found : no posts found" })
+    } else {
+      res.render('post_edit', {post: doc})
     }
   })
 
