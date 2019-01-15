4c8b414b40f474fc31c03abcaa5307d2d2293fb3
olivier-perrier <olivier.perrier.j@gmail.com>
2019-01-15 23:07:11 +0100

    fix

--- public/comp.js
+++ public/comp.js
@@ -1,8 +1,12 @@
 Vue.component('buttoncounter', {
-    data: function () {
-      return {
-        count: 0
-      }
-    },
-    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
-  })
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: `
+    <button v-on:click="count++">
+      You clicked me {{ count }} times.
+    </button>
+    `
+})
