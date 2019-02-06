2926de59deada7a7b0e1e6d923ab86b58b9b48ca
PERRIER Olivier <olivier.perrier@soprasteria.com>
2019-02-06 17:35:23 +0100

    ihm improvement

--- public/widgetposts.js
+++ public/widgetposts.js
@@ -1,4 +1,11 @@
-export default {
+Vue.component('widgetposts', {
+  template: `
+  <div>
+        <h3>Lastest posts</h3>
+        <div v-for="post in posts">
+            <a :href="'post/' + post._id">{{post.title}}</a>
+        </div>
+    </div>`,
 
   data() {
     return {
@@ -13,15 +20,5 @@ export default {
     })
   },
 
-  template: /*html*/`
 
-    <div>
-      <h2>Lastest posts</h2>
-      <div v-for="post in posts">
-        <a :href="'post/' + post._id">{{post.title}}</a>
-      </div>
-    </div>`
-
-
-}
-// )
+})
