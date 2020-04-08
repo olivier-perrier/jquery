e9ffa7c918d4ba12548c5e79c6b8150d53fae049
olivier-perrier <olivier.perrier.j@gmail.com>
2020-04-08 18:31:05 +0200

    moving data to controllers

---
+++ models/index.js
@@ -0,0 +1,3 @@
+// Il n'y a pas lieu d'avoir de modeles pour les types paramétrés génériques.
+// Peut etre dans le future un modele pour un type spécifique système.
+// Peut etre dans le future un modele de base pour l'installation des types prédéfinit au départ de l'application.
